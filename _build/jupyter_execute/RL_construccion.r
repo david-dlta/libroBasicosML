37736759516512037396dfba379503af0557b905
david-dlta <54393717+david-dlta@users.noreply.github.com>
2020-11-19 19:41:58 +0100

    modificado _toc resueltos problemas tabulador

--- _build/jupyter_execute/RL_construccion.r
+++ _build/jupyter_execute/RL_construccion.r
@@ -41,30 +41,10 @@ b <- as.numeric(b)
 m <- modelo_df %>%
             select(V2)
 
-m <- as.numeric(x)
-
-
-#punto_0 <- modelo_df[1] + modelo_df[2] * 2.5
-
-#punto_1 <- modelo_df[1] + modelo_df[2] * 25
-
-df <- as.data.frame(t(p))
+m <- as.numeric(m)
 
 ggplot() +
     geom_point(data = cars, aes (x = speed, y = dist)) +
     geom_line(data = NULL, aes(x= c(5,25) , y=c((5 * m) + b, (25 * m) + b)),color='red')
 
-ggplot(data = df, aes(x= c(0, 0 * x + b) , y=c(25, 25 * x + b))) + geom_line()
 
-datamatrix = as.data.frame(t(p), stringsAsFactors = FALSE)
-
-class(datamatrix)
-
-datamatrix[1]
-
-ggplot(DF,aes(Day, Measure)) + 
-    geom_point() + 
-    stat_summary(fun.y = "mean", geom="line")
-
-
-geom_line(aes((modelo[1] + (modelo[2] * 2.5)) ,(modelo[1] + modelo[2] * 25),color='red'))
